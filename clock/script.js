bbefd8aed8ac03ca240c8f70bb71540c80280cf8
Zhu Wentao <95064358+taoo0316@users.noreply.github.com>
2022-06-22 18:52:11 +0800

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRation = currentDate.getSeconds()/60
+    const minutesRation = (secondsRation + currentDate.getMinutes())/60
+    const hoursRation = (minutesRation + currentDate.getHours())/12
+    setRotation(hourHand, hoursRation)
+    setRotation(minuteHand, minutesRation)
+    setRotation(secondHand, secondsRation)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
